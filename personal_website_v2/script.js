b0b10c1d777acb633d3f5bf65d1209a5d53a48b8
german salcedo <leospillane2020@outlook.com>
2023-08-07 12:41:55 -0500

    adfas

---
+++ personal_website_v2/script.js
@@ -0,0 +1,7 @@
+let profiles = document.getElementsByClassName("profile");
+for(let i=0; i<profiles.length; i++) {
+    let img = profiles[i].getElementsByTagName("img")[0]; // Get the first image inside the profile div
+    img.style.width = "300px";
+    img.style.height = "300px";
+    img.style.border = "7px dashed blue";
+}
